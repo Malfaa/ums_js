5fa8aa0fdd898d5d91c52dfdb2487d8bce303385
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2024-10-03 18:01:22 +0000

    feat & rebase: adição de TS, mundança em layout

---
+++ js/script.ts
@@ -0,0 +1,15 @@
+function itemDeLista(){
+    /* 
+    ideia é montar o html aqui para quando for
+    adicionar ou atualizar a lista do Banco de dados
+    vindo do firebase, 
+    */
+}
+
+function adicionarUsuarios(){
+    
+}
+
+function atualizarUsuarios(){
+
+}
