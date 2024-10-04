333409beebb4d0f6eb5b7ed15c04ec60b31f95e7
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2024-10-04 16:33:34 +0000

    feat: adição de novos ícones, novo media query e de alguns scripts

--- js/script.ts
+++ js/script.ts
@@ -13,3 +13,20 @@ function adicionarUsuarios(){
 function atualizarUsuarios(){
 
 }
+/*
+const button = document.getElementById("botoes-input");
+function trocaTypeViaScreenSize(){
+    if (window.innerWidth < 860){
+        button.type = "image";
+        //input.setAttribute("type", "image");
+        button.src = "/res/images/adicionar_bttn.svg";
+        //input.setAttribute("src", "/res/images/adicionar_bttn.svg")
+        button.innerHTML = "<img src='/res/images/adicionar_bttn.svg' height: '50' width: '40'>";
+    }else{
+        button.innerHTML = "Adicionar";
+    }
+}
+
+window.addEventListener("resize", trocaTypeViaScreenSize);
+
+trocaTypeViaScreenSize();*/
