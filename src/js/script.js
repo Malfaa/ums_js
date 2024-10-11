ff6106bc30638827040a47e6f2c06dacc6cbbbda
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2024-10-11 18:17:27 +0000

    refactor: alterando import do firebase

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";//'firebase/app'
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";//"firebase/auth";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";//"firebase/firestore";
+//TODO firebase in project
 import * as repositorio from "/src/js/repositorio/repositorio.js";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 /*
 
     //Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
